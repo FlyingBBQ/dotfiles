16457eac14cda58a59d3fdebf294f48b46ab25f6
FlyingBBQ <derek.cardol@gmail.com>
2017-12-05 20:28:29 +0100

    wm update

---
+++ WM/i3/desktop/i3/set_workspaces.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sleep 0.5
+lock.sh &
+
+sleep 0.5
+discord &
+
+sleep 2
+i3-msg 'workspace 2:|2| ; append_layout ~/.config/i3/workspaces/workspace-2-qt.json'
+qutebrowser &
+
+sleep 0.5
+i3-msg 'workspace 3:|3| ; append_layout ~/.config/i3/workspaces/workspace-3_clock.json'
+
+# terminals workspace 3
+urxvt -name topleft -e 'ncmpcpp' &
+urxvt -name bottomleft -e 'cava' &
+urxvt -name topright -e bash -c "alsi -l -u && bash" &
+urxvt -name centerright -e bash -c "tty-clock -s -b -c -C 3" &
+urxvt -name bottomright -e bash -c "htop -u derek" &
