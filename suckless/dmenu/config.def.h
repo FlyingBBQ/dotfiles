08dd356c132b120f1c2bc770bc68073218e7a74b
FlyingBBQ <derek.cardol@gmail.com>
2018-07-19 17:00:24 +0200

    Lineheight patch

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -15,6 +15,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
--- suckless/dmenu/config.h
+++ suckless/dmenu/config.h
@@ -15,6 +15,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+static unsigned int lineheight = 25;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
