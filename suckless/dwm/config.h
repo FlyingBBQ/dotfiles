bccc4c901f5ec5d6d9f2ef058cab0bcc323b356c
Derek Cardol <derek.cardol@gmail.com>
2019-01-21 08:41:31 +0100

    dwm keybindings; no iso mode in urxvt

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -88,7 +88,7 @@ static Key keys[] = {
     { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
     { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
     { MODKEY,                       XK_f,      zoom,           {0} },
-    { MODKEY,                       XK_Tab,    view,           {0} },
+    { MODKEY,                       XK_grave,  view,           {0} },
     { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
     { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
     { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[1]} },
@@ -97,10 +97,11 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
     { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-    { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-    { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-    { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-    { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    { MODKEY,                       XK_z,      focusmon,       {.i = -1 } },
+    { MODKEY,                       XK_x,      focusmon,       {.i = +1 } },
+    { MODKEY,                       XK_Tab,    focusmon,       {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_z,      tagmon,         {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_x,      tagmon,         {.i = +1 } },
     TAGKEYS(                        XK_1,                      0)
     TAGKEYS(                        XK_2,                      1)
     TAGKEYS(                        XK_3,                      2)
