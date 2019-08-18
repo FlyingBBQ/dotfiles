18b70f79cfd6c8e44fb7745b5cda937d55435517
FlyingBBQ <derek.cardol@gmail.com>
2019-08-18 22:34:45 +0200

    enable/disable of modules

--- suckless/status.sh
+++ suckless/status.sh
@@ -4,29 +4,38 @@
 #
 
 status_fifo=/tmp/status-fifo
-sep="#1 #4"
 
 # make fifo file
 [ -e "$status_fifo" ] && rm "$status_fifo"
 mkfifo "$status_fifo"
 
+# Enable/Disable modules
+mod_date=1
+mod_time=1
+mod_mem=1
+mod_cpu=1
+mod_bat=1
+mod_ssid=0
+mod_temp=0
+mod_mail=0
+
 ## Modules
 # date
-while :
+((mod_date)) && while :
 do
     echo 'D'$(date "+%a %d %b");
     sleep 60;
 done > $status_fifo &
 
 # time
-while :
+((mod_time)) && while :
 do
     echo 'H'$(date "+%T");
     sleep 1;
 done > $status_fifo &
 
 # memory
-while :
+((mod_mem)) && while :
 do
     perc=$(free -m | grep Mem | awk '{printf "%2.f", $3/$2 * 100.0}' | cut -d "%" -f1)
     if [ $perc -gt 85 ]; then
@@ -39,14 +48,14 @@ do
 done > $status_fifo &
 
 # cpu
-while :
+((mod_cpu)) && while :
 do
     echo 'C'$(awk -v a="$(awk '/cpu /{print $2+$4,$2+$4+$5}' /proc/stat; sleep 0.5)" '/cpu /{split(a,b," "); printf "%2.f", 100*($2+$4-b[1])/($2+$4+$5-b[2]) }'  /proc/stat)
     sleep 1;
 done > $status_fifo &
 
 # battery
-while :
+((mod_bat)) && while :
 do
     bat=""
     stat=$(acpi | awk '{print $3}' | sed 's/,$//')
@@ -71,36 +80,36 @@ do
 done > $status_fifo &
 
 # SSID
-#while :
-#do
-#    if [ "$(nmcli radio wifi)" == "enabled" ] && [ -n "$(iwgetid -r)" ]; then
-#        echo "S$(iwgetid -r)"
-#    else
-#        echo "Sdisconnected"
-#    fi
-#    sleep 3
-#done > $status_fifo &
+((mod_ssid)) && while :
+do
+    if [ "$(nmcli radio wifi)" == "enabled" ] && [ -n "$(iwgetid -r)" ]; then
+        echo "S$(iwgetid -r)"
+    else
+        echo "Sdisconnected"
+    fi
+    sleep 3
+done > $status_fifo &
 
 # temp
-#while :
-#do
-#    echo 'T'$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1/1000}')
-#    sleep 3;
-#done > $status_fifo &
+((mod_temp)) && while :
+do
+    echo 'T'$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1/1000}')
+    sleep 3;
+done > $status_fifo &
 
 # email
-#while :
-#do
-#    echo 'E'$(python ~/code/scripts/.mail)
-#    sleep 10;
-#done > $status_fifo &
+((mod_mail)) && while :
+do
+    echo 'E'$(python ~/code/scripts/.mail)
+    sleep 10;
+done > $status_fifo &
 
 
 ## bar
 while read -r line ; do
     case $line in
         D*)
-            date="${line#?}"
+            date="#4 ${line#?}"
             ;;
         H*)
             clock="#3 ${line#?}"
@@ -124,7 +133,7 @@ while read -r line ; do
             ssid="WiFi #5${line#?}"
             ;;
     esac
-    xsetroot -name  " $cpu #1 $mem #1 $battery $sep $date #1 $clock "
+    xsetroot -name  " $cpu #1 $mem #1 $battery #1 $date #1 $clock "
 
 done < "$status_fifo"
 
