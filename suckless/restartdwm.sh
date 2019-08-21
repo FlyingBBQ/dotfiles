1136d0c72c487f1d72f6a36ea95866e2b3aacac0
FlyingBBQ <derek.cardol@gmail.com>
2019-08-21 23:15:22 +0200

    automatically restart dwm if binary changes

---
+++ suckless/restartdwm.sh
@@ -0,0 +1,15 @@
+# relaunch DWM if the binary changes, otherwise bail
+csum=$(sha1sum $(which dwm))
+new_csum=""
+while true
+do
+    if [ "$csum" != "$new_csum" ]
+    then
+        csum=$new_csum
+        dwm
+    else
+        exit 0
+    fi
+    new_csum=$(sha1sum $(which dwm))
+    sleep 0.5
+done
