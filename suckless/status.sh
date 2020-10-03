4454a230f2aa956bd82030078b1aaeca8e528f23
FlyingBBQ <derek.cardol@gmail.com>
2020-10-03 16:33:37 +0200

    Refactor and add argument parsing

--- suckless/status.sh
+++ suckless/status.sh
@@ -1,140 +1,166 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
-# FlyingBBQ @ dwm status
+# FlyingBBQ :: dwm status
 #
 
+# Colors
+c_bg="#1"
+c_fg="#5"
+c_hl="#4"
+c_iv="#3"
+c_gn="#6"
+c_gh="#7"
+c_rn="#8"
+c_rh="#9"
+
+mod_string=""
 status_fifo=/tmp/status-fifo
 
-# make fifo file
+# Make fifo file
 [ -e "$status_fifo" ] && rm "$status_fifo"
 mkfifo "$status_fifo"
 
-# Enable/Disable modules
-mod_date=1
-mod_time=1
-mod_mem=1
-mod_cpu=1
-mod_bat=1
-mod_ssid=0
-mod_temp=0
-mod_mail=0
+usage () {
+cat << EOF
+    Status script for dwm
+    Pass modules in preferred order: status.sh [-m | --module]
 
-## Modules
-# date
-((mod_date)) && while :
-do
-    echo 'D'$(date "+%a %d %b");
-    sleep 60;
-done > $status_fifo &
+    Supported modules:
+        -d, --date
+        -h, --time
+        -m, --memory
+        -c, --cpu
+        -b, --battery
+        -s, --ssid
+        -t, --temp
+        -e, --email
+EOF
+}
 
-# time
-((mod_time)) && while :
-do
-    echo 'H'$(date "+%T");
-    sleep 1;
-done > $status_fifo &
+# Modules
+mod_date () {
+    while :
+    do
+        echo 'D'$(date "+%a %d %b");
+        sleep 60;
+    done > $status_fifo &
+}
 
-# memory
-((mod_mem)) && while :
-do
-    perc=$(free -m | grep Mem | awk '{printf "%2.f", $3/$2 * 100.0}' | cut -d "%" -f1)
-    if [ $perc -gt 85 ]; then
-        mem="#8 RAM #9$perc%"
-    else
-        mem="#4 RAM #5$perc%"
-    fi
-    echo "M$mem"
-    sleep 1;
-done > $status_fifo &
+mod_time () {
+    while :
+    do
+        echo 'H'$(date "+%T");
+        sleep 1;
+    done > $status_fifo &
+}
 
-# cpu
-((mod_cpu)) && while :
-do
-    echo 'C'$(awk -v a="$(awk '/cpu /{print $2+$4,$2+$4+$5}' /proc/stat; sleep 0.5)" '/cpu /{split(a,b," "); printf "%2.f", 100*($2+$4-b[1])/($2+$4+$5-b[2]) }'  /proc/stat)
-    sleep 1;
-done > $status_fifo &
+mod_mem () {
+    while :
+    do
+        perc=$(free -m | grep Mem | awk '{printf "%2.f", $3/$2 * 100.0}' | cut -d "%" -f1)
+        if [ "$perc" -gt 85 ]; then
+            mem="$c_rn RAM $c_rh$perc%"
+        else
+            mem="$c_hl RAM $c_fg$perc%"
+        fi
+        echo "M$mem"
+        sleep 1;
+    done > $status_fifo &
+}
 
-# battery
-((mod_bat)) && while :
-do
-    bat=""
-    stat=$(acpi | awk '{print $3}' | sed 's/,$//')
-    perc=$(acpi | awk '{print $4}' | sed 's/%,$//')
-    case $stat in
-        'Discharging')
-            if [ $perc -lt 20 ]; then
-                bat="#8 BAT #9$perc%"
-            else
-                bat="#4 BAT #5$perc%"
-            fi
-            ;;
-        'Charging')
-            bat="#6 BAT #7$perc%"
-            ;;
-        *)
-            bat="#4 BAT #5$perc"
-            ;;
+mod_cpu () {
+    while :
+    do
+        echo 'C'$(awk -v a="$(awk '/cpu /{print $2+$4,$2+$4+$5}' /proc/stat; sleep 0.5)" '/cpu /{split(a,b," "); printf "%2.f", 100*($2+$4-b[1])/($2+$4+$5-b[2]) }'  /proc/stat)
+        sleep 1;
+    done > $status_fifo &
+}
+
+mod_bat () {
+    while :
+    do
+        bat=""
+        stat=$(acpi | awk '{print $3}' | sed 's/,$//')
+        perc=$(acpi | awk '{print $4}' | sed 's/%,$//')
+        case $stat in
+            'Discharging')
+                if [ "$perc" -lt 20 ]; then
+                    bat="$c_rn BAT $c_rh$perc%"
+                else
+                    bat="$c_hl BAT $c_fg$perc%"
+                fi
+                ;;
+            'Charging')
+                bat="$c_gn BAT $c_gh$perc%"
+                ;;
+            *)
+                bat="$c_hl BAT $c_fg$perc"
+                ;;
+        esac
+        echo "B$bat"
+        sleep 5
+    done > $status_fifo &
+}
+
+mod_ssid () {
+    while :
+    do
+        if [ "$(nmcli radio wifi)" == "enabled" ] && [ -n "$(iwgetid -r)" ]; then
+            echo "S$(iwgetid -r)"
+        else
+            echo "Sdisconnected"
+        fi
+        sleep 3
+    done > $status_fifo &
+}
+
+mod_temp () {
+    while :
+    do
+        echo 'T'$(sensors | awk '/CPU Temperature/ {print $3}' | tr -d +)
+        sleep 3;
+    done > $status_fifo &
+}
+
+mod_mail () {
+    while :
+    do
+        echo 'E'$(python ~/code/scripts/.mail)
+        sleep 10;
+    done > $status_fifo &
+}
+
+# Parse arguments, start modules, and create module string
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -d|--date)      mod_date;  mod_string="${mod_string} \$date" ;;
+        -h|--time)      mod_time;  mod_string="${mod_string} \$clock" ;;
+        -m|--memory)    mod_mem;   mod_string="${mod_string} \$mem" ;;
+        -c|--cpu)       mod_cpu;   mod_string="${mod_string} \$cpu" ;;
+        -b|--battery)   mod_bat;   mod_string="${mod_string} \$battery" ;;
+        -s|--ssid)      mod_ssid;  mod_string="${mod_string} \$ssid" ;;
+        -t|--temp)      mod_temp;  mod_string="${mod_string} \$temp" ;;
+        -e|--email)     mod_email; mod_string="${mod_string} \$clock" ;;
+        *)  echo "Unknown parameter passed: $1"; usage;
+            exit 1 ;;
     esac
-    echo "B$bat"
-    sleep 5
-done > $status_fifo &
+    mod_string="${mod_string} \$c_bg"
+    shift
+done
 
-# SSID
-((mod_ssid)) && while :
-do
-    if [ "$(nmcli radio wifi)" == "enabled" ] && [ -n "$(iwgetid -r)" ]; then
-        echo "S$(iwgetid -r)"
-    else
-        echo "Sdisconnected"
-    fi
-    sleep 3
-done > $status_fifo &
-
-# temp
-((mod_temp)) && while :
-do
-    echo 'T'$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1/1000}')
-    sleep 3;
-done > $status_fifo &
-
-# email
-((mod_mail)) && while :
-do
-    echo 'E'$(python ~/code/scripts/.mail)
-    sleep 10;
-done > $status_fifo &
-
-
-## bar
+# Expand module string with module values
 while read -r line ; do
     case $line in
-        D*)
-            date="#4 ${line#?}"
-            ;;
-        H*)
-            clock="#3 ${line#?}"
-            ;;
-        B*)
-            battery="${line#?}"
-            ;;
-        E*)
-            email="MAIL ${line#?}"
-            ;;
-        M*)
-            mem="${line#?}"
-            ;;
-        C*)
-            cpu="CPU #5${line#?}%"
-            ;;
-        T*)
-            tmp="temp ${line#?}°"
-            ;;
-        S*)
-            ssid="WiFi #5${line#?}"
-            ;;
+        D*) date="$c_hl ${line#?}" ;;
+        H*) clock="$c_iv ${line#?}" ;;
+        B*) battery="${line#?}" ;;
+        E*) email="MAIL ${line#?}" ;;
+        M*) mem="${line#?}" ;;
+        C*) cpu="CPU $c_fg${line#?}%" ;;
+        T*) temp="$c_fg ${line#?}" ;;
+        S*) ssid="WiFi $c_fg${line#?}" ;;
     esac
-    xsetroot -name  " $cpu #1 $mem #1 $battery #1 $date #1 $clock "
-
+    xsetroot -name " $(eval echo "$mod_string")"
 done < "$status_fifo"
 
 wait
